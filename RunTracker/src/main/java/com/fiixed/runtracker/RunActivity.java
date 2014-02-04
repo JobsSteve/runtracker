3586f679be8e6d8983edba1a24205631460b4071
Andrew Bell <fiixed@gmail.com>
2014-02-03 16:37:53 -0800

    initial commit with Location working

---
+++ RunTracker/src/main/java/com/fiixed/runtracker/RunActivity.java
@@ -0,0 +1,22 @@
+package com.fiixed.runtracker;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class RunActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new RunFragment();
+    }
+
+
+}
